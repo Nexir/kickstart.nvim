bc5664c4cd249ad3ca1de381706ddb06ccd4a7a2
NeXiR <nexir49@gmail.com>
2023-10-15 17:59:48 +0200

    Add glow

--- lua/custom/config/remap.lua
+++ lua/custom/config/remap.lua
@@ -1,3 +1,5 @@
 return {
-	vim.keymap.set('n', '<leader>fe', ':Neotree toggle <cr>', { desc = '[F]iles [E]xplorer' })
+	vim.keymap.set('n', '<leader>fe', '<cmd>Neotree toggle<cr>', { desc = '[F]iles [E]xplorer' }),
+	vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "[L]azy" }),
+	vim.keymap.set({ "n", "v" }, "<leader>cf", '<cmd>Format<cr>', { desc = "[C]ode [F]ormat" })
 }
