5a11d22619ddcfadc36acd5c53a10afb5b5042ab
NeXiR <nexir49@gmail.com>
2023-10-20 17:57:24 +0200

    Disable vimwiki

--- lua/custom/plugins/vimwiki.lua
+++ lua/custom/plugins/vimwiki.lua
@@ -1,11 +1,12 @@
 return {
   "vimwiki/vimwiki",
-    init = function()
-      vim.g.vimwiki_list = {
-        { path = '~/vimwiki', syntax = 'markdown', ext = '.md', links_space_char = '-' },
-      }
-      vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
-      vim.g.vimwiki_use_mouse = 1
-      vim.g.vimwiki_markdown_link_ext = 1
-    end
+  enabled = false,
+  init = function()
+    vim.g.vimwiki_list = {
+      { path = '~/vimwiki', syntax = 'markdown', ext = '.md', links_space_char = '-' },
+    }
+    vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
+    vim.g.vimwiki_use_mouse = 1
+    vim.g.vimwiki_markdown_link_ext = 1
+  end
 }
